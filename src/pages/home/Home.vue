17da2a820b36a16ad2a390b4925b72fed0ba9081
liaodehui <1959956996@qq.com>
2019-10-21 01:42:46 +0800

    add-改变目录结构

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <com-header></com-header>
+    <com-content></com-content>
+    <com-footer></com-footer>
+  </div>
+</template>
+
+<script>
+import header from '@/components/home/Header'
+import content from '@/components/home/Content'
+import footer from '@/components/public/Footer'
+export default {
+  components:{
+    'com-header':header,
+    'com-content':content,
+    'com-footer':footer
+  }
+}
+</script>
+
+<style>
+
+</style>
