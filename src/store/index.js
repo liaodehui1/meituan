4e97240da4b4fabc0cbb3261291120e06e99ff83
liaodehui <1959956996@qq.com>
2019-10-25 23:50:53 +0800

    vuex-store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import city from './modules/city'
+import user from './modules/user'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules:{
-    city
+    city,
+    user
   }
 })
