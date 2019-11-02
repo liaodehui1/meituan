73941ac25c7730cf4bed8064c6ebeaa8ff32fc85
liaodehui <1959956996@qq.com>
2019-11-03 00:29:58 +0800

    猜你喜欢

---
+++ src/components/home/container/recommend-container/RecommendCard.vue
@@ -0,0 +1,141 @@
+<template>
+  <router-link :to="`/meishi/${cardInfo.itemId}`" class="link recommend-item">
+    <div class="card-container">
+      <div class="recommend-card-wrapper">
+        <div class="img-box">
+          <img :src="cardInfo.imgUrl" />
+        </div>
+        <div class="info-box">
+          <div class="title aligned ellipsis">{{cardInfo.title}}</div>
+          <div class="score-line aligned">
+            <div class="score">
+              <div class="rate-stars">
+                <ul class="rate-stars-ul rate-stars-dark">
+                  <li class="iconfont icon-star_icon" v-for="index in 5" :key="index"></li>
+                </ul>
+                <!-- 星星计算覆盖 -->
+                <ul class="rate-stars-ul rate-stars-light" :style="`width:${cardInfo.rateWidth}%`">
+                  <li class="iconfont icon-star_icon" v-for="index in 5" :key="index"></li>
+                </ul>
+              </div>
+            </div>
+            <div class="comment-number">{{cardInfo.commentNum}}个评价</div>
+          </div>
+          <div class="desc-line aligne">
+            <div class="desc ellipsis">{{cardInfo.areaName}}</div>
+          </div>
+          <div class="bottom-line aligned">
+            <div class="price-box">
+              <span>
+                <span class="yuan">￥</span>
+                <span class="price-number numfont">{{cardInfo.lowPrice}}</span>
+                <span class="price-desc">起</span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props:['cardInfo']
+};
+</script>
+
+<style lang="stylus" scoped>
+  .recommend-item
+    width 20%
+    .card-container
+      padding 10px
+      &:hover
+        background-color #f4f4f4
+        transition background-color .5s
+        border-radius 4px
+      .img-box
+        height 0
+        padding-bottom 56.25%
+        background-color #f4f4f4
+        background-image url(/assets/images/214b8411190e874781fe91dd3096236211567.png)
+        background-size cover
+        background-position 50% 50%
+        border-radius 4px
+        overflow hidden
+        img 
+          width 100%
+      .info-box
+        position relative
+        margin-top 10px
+        .title
+          color #222222
+          margin 0
+          font-weight 500
+          &.aligned
+            font-size 16px
+            height 22px
+            line-height 22px
+        .score-line
+          div
+            display inline-block
+          &.aligned
+            height 24px
+          .score
+            display inline-block
+            .rate-stars
+              width 84px
+              height 24px
+              position relative
+              display inline-block
+              .rate-stars-ul
+                padding 0
+                text-align left
+                margin 3px 0
+                white-space nowrap
+                &>li
+                  display inline-block
+                  padding 0px 2px
+              .rate-stars-dark
+                color #c3c3c3
+              .rate-stars-light
+                color #ff9900
+                position absolute
+                top 0
+                overflow hidden
+          .comment-number
+            margin-left 10px
+            height 24px
+            line-height 24px
+            color #999999
+            font-size 12px
+        .desc-line 
+          &.aligne
+            height 18px
+            .desc
+              font-size 12px
+              color #999999
+              height 18px
+              line-height 18px
+              margin-top 4px
+        .bottom-line
+          position relative
+          div
+            display inline-block
+          .price-box
+            color #ff6600
+            font-size 0
+            .yuan
+              font-size 14px
+              font-weight 700
+            .price-number
+              font-size 22px
+              letter-spacing -0.5px
+            .price-desc
+              font-size 12px
+              margin-left 4px
+              font-weight 700
+  @media screen and (max-width 1200px)
+    .recommend-item
+      width 25%
+</style>
---
+++ src/components/home/container/recommend-container/RecommendContainer.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <m-nav :nav="nav" :styl="styl"></m-nav>
+    <panel :className="className">
+      <recommend-card v-for="(item,index) in itemList" :key="index" :cardInfo="item"></recommend-card>
+    </panel>
+  </div>
+</template>
+
+<script>
+import Nav from '../Nav'
+import Panel from '../Panel'
+import RecommendCard from './RecommendCard'
+export default {
+  data() {
+    return {
+      className:'recommend-panel',
+      nav:{
+        theme:'猜你喜欢',
+        navList:[{
+          text:'为你甄选最适合的'
+        }]
+      },
+      itemList:null,
+      styl:'background: linear-gradient(to right, rgb(88, 174, 221) 2%, rgb(66, 191, 205) 97%) rgb(88, 174, 221);'
+    }
+  },
+  components:{
+    'm-nav':Nav,
+    Panel,
+    RecommendCard
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData(){
+      fetch(`http://127.0.0.1:8080/static/json/recommend/recommends.json`)
+        .then(res => res.json())
+        .then(res => {
+          // console.log(res)
+          if(res.length){
+            res = res.map(item => {
+              if(item.imgUrl.indexOf('w.h/') !== -1){
+                item.imgUrl = item.imgUrl.replace('w.h/','')
+              }
+              item.rateWidth = item.score / 5.0 * 100
+              return item
+            })
+            this.itemList = res
+          }
+        })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
