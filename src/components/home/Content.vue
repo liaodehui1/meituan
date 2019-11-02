19d2dea24cca1804e578c8ade91752bf83f6bd3f
liaodehui <1959956996@qq.com>
2019-11-03 00:25:53 +0800

    中间内容

--- src/components/home/Content.vue
+++ src/components/home/Content.vue
@@ -5,6 +5,21 @@
         <left-banner></left-banner>
         <right-banner></right-banner>
       </div>
+      <div class="quality-container">
+        <quality-container></quality-container>
+      </div>
+      <div class="offer-container">
+        <cheap-container></cheap-container>
+      </div>
+      <div class="maoyan-container">
+        <maoyan-container></maoyan-container>
+      </div>
+      <div class="zhenguo-container">
+        <zhenguo-container></zhenguo-container>
+      </div>
+      <div class="recommend-container">
+        <recommend-container></recommend-container>
+      </div>
     </div>
   </div>
 </template>
@@ -12,10 +27,20 @@
 <script>
 import LeftBanner from './left-banner/LeftBanner'
 import RightBanner from './right-banner/RightBanner'
+import QualityContainer from './container/quality-container/QualityContainer'
+import CheapContainer from './container/offer-container/CheapContainer'
+import ZhenguoContainer from './container/zhenguo-container/ZhenguoContainer'
+import RecommendContainer from './container/recommend-container/RecommendContainer'
+import MaoyanContainer from './container/maoyan-container/MaoyanContainer'
 export default {
   components:{
     LeftBanner,
-    RightBanner
+    RightBanner,
+    QualityContainer,
+    CheapContainer,
+    ZhenguoContainer,
+    RecommendContainer,
+    MaoyanContainer
   }
 }
 </script>
@@ -26,15 +51,17 @@ export default {
     .index-container
       margin 0 auto
       width 1190px
-      .banner-container,.scenes-container,.maoyan-container,.zhenguo-container,.recommend-container
+      .banner-container,.quality-container,.offer-container,.maoyan-container,.zhenguo-container,.recommend-container
         width 1190px
-
+        position relative
+        display inline-block //去除容器塌陷
       .banner-container
         margin 0 auto
-        position relative
+      .quality-container,.offer-container,.maoyan-container,.zhenguo-container,.recommend-container
+        margin 40px auto 0
     @media screen and (max-width 1200px)
       .index-container
         width 940px
-      .banner-container,.scenes-container,.maoyan-container,.zhenguo-container,.recommend-container
-        width 950px
+        .banner-container,.quality-container,.offer-container,.maoyan-container,.zhenguo-container,.recommend-container
+          width 950px
 </style>
