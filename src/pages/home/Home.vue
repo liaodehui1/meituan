bc181f05bd252559c0a145444800e1c5d7b243e1
liaodehui <1959956996@qq.com>
2019-11-04 23:25:02 +0800

    add-footer

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <com-header></com-header>
     <com-content></com-content>
+    <com-nav></com-nav>
     <com-footer></com-footer>
   </div>
 </template>
@@ -10,10 +11,12 @@
 import header from '@/components/home/Header'
 import content from '@/components/home/Content'
 import footer from '@/components/public/Footer'
+import nav from '@/components/home/Nav'
 export default {
   components:{
     'com-header':header,
     'com-content':content,
+    'com-nav':nav,
     'com-footer':footer
   }
 }
