acc449e39931474b6b244c787b8a55bac3ed0fe8
liaodehui <1959956996@qq.com>
2019-11-11 22:09:25 +0800

    add-axios,懒加载

--- src/main.js
+++ src/main.js
@@ -4,12 +4,24 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
+import VueLazyload  from 'vue-lazyload'
 import 'element-ui/lib/theme-chalk/index.css';
 import store from './store'
 import './assets/font/index.css';
+
 Vue.use(ElementUI)
 Vue.config.productionTip = false
-
+// 图片懒加载
+Vue.use(VueLazyload,{
+  preLoad:0.5,
+  attempt:2,
+  listenEvents:['scroll'],
+  observer: true,
+  observerOptions:{ 
+      rootMargin: '-10px', 
+      threshold: 0.1 ,
+  }
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
