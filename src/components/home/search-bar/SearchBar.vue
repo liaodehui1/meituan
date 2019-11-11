b9043fbcdd2a7355ca986cc00dd0af766f7068a1
liaodehui <1959956996@qq.com>
2019-11-11 22:06:59 +0800

    头部搜索

--- src/components/home/search-bar/SearchBar.vue
+++ src/components/home/search-bar/SearchBar.vue
@@ -2,9 +2,11 @@
   <el-row class="header-content">
     <el-col :span="6">
       <div  class="header-title-module">
-        <router-link to="/" >
-          <img src="//s0.meituan.net/bs/fe-web-meituan/fa5f0f0/img/logo.png" alt="美团" />
-        </router-link>
+        <div class="header-title">
+          <router-link to="/" >
+            <img src="//s0.meituan.net/bs/fe-web-meituan/fa5f0f0/img/logo.png" alt="美团" />
+          </router-link>
+        </div>
       </div>
     </el-col>
     <el-col :span="12" class="header-search-module">
@@ -23,18 +25,5 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.header-title-module
-  padding-top 28px
-  padding-right 60px
-  padding-bottom 40px
-  height 54px
-  img 
-    width 126px
-    height 46px
-.header-search-module
-  padding-top 28px
-@media screen and (max-width 1200px)
-  .header-title-module
-    padding-right 40px
-    min-width 200px
+
 </style>
