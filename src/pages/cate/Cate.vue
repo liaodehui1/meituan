5245d4cc03559da862a0d2d4af6e65521a2a65d4
liaodehui <1959956996@qq.com>
2019-11-14 22:37:39 +0800

    店铺细节

---
+++ src/pages/cate/Cate.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="main" v-if="cateInfo">
+    <com-header :type="cateInfo.poiInfo.crumbs[0].title"></com-header>
+    <com-content :cateInfo="cateInfo"></com-content>
+  </div>
+</template>
+
+<script>
+import header from '@/components/cate/Header'
+import content from '@/components/cate/Content'
+import api from '@/api/detail'
+export default {
+  data() {
+    return {
+      cateInfo:null
+    }
+  },
+  components:{
+    'com-header':header,
+    'com-content':content
+  },
+  created() {
+    // console.log(this.$route)
+    let id = this.$route.params.id
+    api.cate(id)
+      .then(res => {
+        console.log(res)
+        this.cateInfo = res
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
