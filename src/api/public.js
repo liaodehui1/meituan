08399622e2076a55f39300de3bebcd53b621c03f
liaodehui <1959956996@qq.com>
2019-11-18 23:13:52 +0800

    add-recommendList

--- src/api/public.js
+++ src/api/public.js
@@ -3,5 +3,8 @@ import {fetchGet} from './index';
 export default {
   suggest(params) {
     return fetchGet('/public/suggest',params)
+  },
+  recommendList(params) {
+    return fetchGet('/public/recmmendList',params)
   }
 }
